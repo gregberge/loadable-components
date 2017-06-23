6c07a444eb80bd2ccb1a1e2586dc1e2d9c73a487
Greg Bergé <berge.greg@gmail.com>
2017-06-23 15:59:16 +0200

    refactor: rename exposed constant

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 import createSymbol from './utils/createSymbol'
 
-export const COMPONENT_IDS = '__DEFER_COMPONENT_IDS__'
+export const COMPONENT_IDS = '__LOADABLE_COMPONENT_IDS__'
 export const LOADABLE = createSymbol('loadable')
