ba90289206dd4e3419c51622b7ab6aa34004e0bd
Greg Bergé <berge.greg@gmail.com>
2018-11-23 16:17:18 +0100

    fix(server): fix url join (#166)

--- packages/server/src/util.test.js
+++ packages/server/src/util.test.js
@@ -6,20 +6,27 @@ describe('util', () => {
       expect(joinURLPath('public', 'style.css')).toBe('public/style.css')
       expect(joinURLPath('public/', 'style.css')).toBe('public/style.css')
     })
+
+    it('should join paths starting with "/"', () => {
+      expect(joinURLPath('/foo', 'style.css')).toBe('/foo/style.css')
+      expect(joinURLPath('/', 'style.css')).toBe('/style.css')
+    })
+
     it('should join paths with absolute public path', () => {
       const publicPath = 'http://localhost:3001/public'
 
       expect(joinURLPath(publicPath, 'style.css')).toBe(
-        `${publicPath}/style.css`,
+        `http://localhost:3001/public/style.css`,
       )
       expect(joinURLPath(`${publicPath}/`, 'style.css')).toBe(
-        `${publicPath}/style.css`,
+        `http://localhost:3001/public/style.css`,
       )
     })
+
     it('should join paths with protocol free public path', () => {
       const publicPath = '//127.0.0.1/public'
       expect(joinURLPath(publicPath, 'style.css')).toBe(
-        `${publicPath}/style.css`,
+        `//127.0.0.1/public/style.css`,
       )
     })
   })
