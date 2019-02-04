4f832dc7d4e8ea234c4920ffe4588b8647c05f6a
Victor <victor@turo.com>
2019-02-04 10:29:22 -0800

    feat(babel-plugin): transform code annotated with magic comment
    Transform imports inside code annotated with `/* #__LOADABLE__ * /`.
    
    Fixes #192

--- packages/babel-plugin/src/index.js
+++ packages/babel-plugin/src/index.js
@@ -13,6 +13,8 @@ const properties = [
   resolveProperty,
 ]
 
+const LOADABLE_COMMENT = '#__LOADABLE__'
+
 const loadablePlugin = api => {
   const { types: t } = api
 
@@ -42,34 +44,74 @@ const loadablePlugin = api => {
     )
   }
 
+  function hasLoadableComment(path) {
+    const comments = path.get('leadingComments')
+    const comment = comments.find(
+      ({ node }) =>
+        node && node.value && String(node.value).includes(LOADABLE_COMMENT),
+    )
+    if (!comment) return false
+    comment.remove()
+    return true
+  }
+
+  function getFuncPath(path) {
+    const funcPath = path.isCallExpression() ? path.get('arguments.0') : path
+    if (
+      !funcPath.isFunctionExpression() &&
+      !funcPath.isArrowFunctionExpression() &&
+      !funcPath.isObjectMethod()
+    ) {
+      return null
+    }
+    return funcPath
+  }
+
+  function transformImport(path) {
+    const callPaths = collectImportCallPaths(path)
+
+    // Ignore loadable function that does not have any "import" call
+    if (callPaths.length === 0) return
+
+    // Multiple imports call is not supported
+    if (callPaths.length > 1) {
+      throw new Error(
+        'loadable: multiple import calls inside `loadable()` function are not supported.',
+      )
+    }
+
+    const [callPath] = callPaths
+
+    const funcPath = getFuncPath(path)
+    if (!funcPath) return
+
+    funcPath.node.params = funcPath.node.params || []
+
+    const object = t.objectExpression(
+      propertyFactories.map(getProperty =>
+        getProperty({ path, callPath, funcPath }),
+      ),
+    )
+
+    if (funcPath.isObjectMethod()) {
+      funcPath.replaceWith(
+        t.objectProperty(funcPath.node.key, object, funcPath.node.computed),
+      )
+    } else {
+      funcPath.replaceWith(object)
+    }
+  }
+
   return {
     inherits: syntaxDynamicImport,
     visitor: {
       CallExpression(path) {
         if (!isValidIdentifier(path)) return
-
-        const callPaths = collectImportCallPaths(path)
-
-        // Ignore loadable function that does not have any "import" call
-        if (callPaths.length === 0) return
-
-        // Multiple imports call is not supported
-        if (callPaths.length > 1) {
-          throw new Error(
-            'loadable: multiple import calls inside `loadable()` function are not supported.',
-          )
-        }
-
-        const [callPath] = callPaths
-        const funcPath = path.get('arguments.0')
-
-        funcPath.replaceWith(
-          t.objectExpression(
-            propertyFactories.map(getProperty =>
-              getProperty({ path, callPath, funcPath }),
-            ),
-          ),
-        )
+        transformImport(path)
+      },
+      'ArrowFunctionExpression|FunctionExpression|ObjectMethod': path => {
+        if (!hasLoadableComment(path)) return
+        transformImport(path)
       },
     },
   }
