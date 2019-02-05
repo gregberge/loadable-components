578107e2d07807b789e04dee7cd30e730226ac7d
Greg Bergé <greg@smooth-code.com>
2019-02-06 00:27:05 +0100

    feat(component): add preload method
    Closes #196

--- packages/component/src/resolvers.js
+++ packages/component/src/resolvers.js
@@ -3,8 +3,7 @@ import hoistNonReactStatics from 'hoist-non-react-statics'
 export function resolveComponent(loadedModule, { Loadable }) {
   const Component = loadedModule.default || loadedModule
   hoistNonReactStatics(Loadable, Component, {
-    prefetch: true,
-    Prefetch: true,
+    preload: true,
   })
   return Component
 }
