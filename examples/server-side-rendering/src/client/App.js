4cab4f9beaf0bd440be5b691d4b923a4782d57e5
Greg Bergé <berge.greg@gmail.com>
2019-02-05 23:47:25 +0100

    feat(server): add option to disable SSR (#223)
    Closes #195

--- examples/server-side-rendering/src/client/App.js
+++ examples/server-side-rendering/src/client/App.js
@@ -7,6 +7,7 @@ const A = loadable(() => import('./letters/A'))
 const B = loadable(() => import('./letters/B'))
 const C = loadable(() => import(/* webpackPreload: true */ './letters/C'))
 const D = loadable(() => import(/* webpackPrefetch: true */ './letters/D'))
+const E = loadable(() => import('./letters/E'), { ssr: false })
 const X = loadable(props => import(`./letters/${props.letter}`))
 
 // We keep some references to prevent uglify remove
@@ -18,8 +19,13 @@ const Moment = loadable.lib(() => import('moment'))
 const App = () => (
   <div>
     <A />
+    <br />
     <B />
+    <br />
     <X letter="A" />
+    <br />
+    <E />
+    <br />
     <Moment>{moment => moment().format('HH:mm')}</Moment>
   </div>
 )
