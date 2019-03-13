97ff6ac107ed4ffdd4dd03103c37ce394e4ddaf5
ambar <ambar.lee@gmail.com>
2019-03-13 22:10:54 +0800

    perf(build): add build target for Node (#267)

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
+let targets = {}
+
+if (process.env.BUILD_TARGET === 'node') {
+  targets = { node: '8' }
+}
+
 module.exports = {
-  presets: ['@babel/preset-react', ['@babel/preset-env', { loose: true }]],
+  presets: [
+    ['@babel/preset-react', { useBuiltIns: true }],
+    ['@babel/preset-env', { loose: true, targets }],
+  ],
   plugins: ['@babel/plugin-proposal-class-properties'],
 }
