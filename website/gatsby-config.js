1b1f0145f1dbd08ce73a73ea6f737118ac40d94d
Greg Bergé <greg@smooth-code.com>
2019-04-23 11:38:35 +0200

    docs: upgrade website

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -1,10 +1,14 @@
-const { getGatsbyConfig } = require('smooth-doc/config')
-
-module.exports = getGatsbyConfig({
-  root: __dirname,
-  name: 'Loadable Components',
-  slug: 'loadable-components',
-  github: 'https://github.com/smooth-code/loadable-components',
-  menu: ['Introduction', 'Guides', 'API'],
-  nav: [{ title: 'Docs', url: '/docs/' }],
-})
+module.exports = {
+  __experimentalThemes: [
+    {
+      resolve: require.resolve('smooth-doc'),
+      options: {
+        name: 'Loadable Components',
+        slug: 'loadable-components',
+        github: 'https://github.com/smooth-code/loadable-components',
+        menu: ['Introduction', 'Guides', 'API'],
+        nav: [{ title: 'Docs', url: '/docs/' }],
+      },
+    },
+  ],
+}
