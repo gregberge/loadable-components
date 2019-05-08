8c07caec99ae5fe2b941c34dd1d08147cdd561e5
Greg Bergé <greg@smooth-code.com>
2019-05-08 21:47:27 +0200

    feat: support reactive dynamic loadable
    Closes #284

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,14 @@
-let targets = {}
-
-if (process.env.BUILD_TARGET === 'node') {
-  targets = { node: '8' }
+function getTargets() {
+  if (process.env.BUILD_TARGET === 'node') {
+    return { node: '8' }
+  }
+  return undefined
 }
 
 module.exports = {
   presets: [
     ['@babel/preset-react', { useBuiltIns: true }],
-    ['@babel/preset-env', { loose: true, targets }],
+    ['@babel/preset-env', { loose: true, targets: getTargets() }],
   ],
   plugins: ['@babel/plugin-proposal-class-properties'],
 }
