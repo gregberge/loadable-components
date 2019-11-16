6748e39bc2a25ffdb899af4576328105345c2cb4
Jacky Efendi <jacky.efendi1@gmail.com>
2019-11-16 16:31:44 +0700

    chore: rename package to loadable-codemod

--- packages/codemod/bin/main.js
+++ packages/codemod/bin/main.js
@@ -50,10 +50,10 @@ try {
   if (err.type === 'Invalid params') {
     console.error('Invalid params passed!')
     console.error(
-      '@loadable/codemod requires 2 params to be passed, the name of the codemod, and a directory to apply the codemod to.',
+      'loadable-codemod requires 2 params to be passed, the name of the codemod, and a directory to apply the codemod to.',
     )
     console.error(
-      'Example: npx @loadable/codemod react-loadable-to-loadable-component ./src/client',
+      'Example: npx loadable-codemod react-loadable-to-loadable-component ./src/client',
     )
 
     process.exit(1)
