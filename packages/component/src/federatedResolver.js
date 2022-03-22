b05966077acb2e1356f92452dbcb67ef93651da7
Иван Малюгин <ivanxru@mail.ru>
2022-03-22 18:29:10 +0300

    Fix issue caused by inaccurate code copying

--- packages/component/src/federatedResolver.js
+++ packages/component/src/federatedResolver.js
@@ -3,20 +3,20 @@ const localModuleNames = [];
 
 const isFederated = (loadableCtor) => loadableCtor.resolve().indexOf('webpack/container/remote') === 0;
 
-export const register = (loadableCtor) => {
+export function register(loadableCtor) {
   const moduleName = loadableCtor.chunkName();
   if (isFederated(loadableCtor)) {
     federatedModuleMap[moduleName] = loadableCtor.importAsync;
   } else {
     localModuleNames.push(moduleName);
   }
-};
+}
 
 /**
  * Calls importAsync for all the modules listed in moduleNames
  * Resolves after modules are loaded
  */
-export const resolveNamedFederatedModules = (moduleNames) => {
+export function resolveNamedFederatedModules(moduleNames) {
   const federatedModules = moduleNames
     .filter((name) => !localModuleNames.includes(name))
     .map((name) => federatedModuleMap[name]);
@@ -25,12 +25,15 @@ export const resolveNamedFederatedModules = (moduleNames) => {
   federatedModuleMap = {};
 
   return Promise.all(federatedModules.map((loader) => loader()));
-};
+}
 
 
 /**
  * Preload all the existing federated modules, to be used on the server (we don't have loadable required chunks on SSR)
  */
-export const resolveAllFederatedModules = () => resolveNamedFederatedModules(Object.keys(federatedModuleMap))
-  // repeat if more modules are discovered during preloading
-  .then(Object.keys(federatedModuleMap).length ? resolveAllFederatedModules() : undefined);
+export function resolveAllFederatedModules() {
+  const moduleNames = Object.keys(federatedModuleMap);
+  return moduleNames.length ? resolveNamedFederatedModules(moduleNames)
+    // repeat if more modules are discovered during preloading
+    .then(resolveAllFederatedModules) : Promise.resolve();
+}
