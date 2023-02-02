74c49a040960ade7a20be3db714e515ae9e917cd
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2023-02-03 00:11:12 +0100

    Add more cypress tests

--- examples/server-side-rendering/cypress/tests/spec.cy.js
+++ examples/server-side-rendering/cypress/tests/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/', {
+  it('passes with simple', () => {
+    cy.visit('/?entrypoint=simple', {
     })
     cy.window().then((win) => {
       cy.spy(win, 'loadableEvents').as('spyloadableEvents')
@@ -8,6 +8,30 @@ describe('template spec', () => {
   
 
     cy.get('@spyloadableEvents')
-      .should('be.calledWith', Cypress.sinon.match({ type: 'successAsyncLoad' }));
+      .should('be.calledWith', Cypress.sinon.match({ type: 'loadSync', chunkName: 'letters-A' }));
+  })
+  it('passes with props', () => {
+    cy.visit('/?entrypoint=props', {
+    })
+    cy.window().then((win) => {
+      cy.spy(win, 'loadableEvents').as('spyloadableEvents')
+    })
+  
+
+    cy.get('@spyloadableEvents')
+      .should('be.calledWith', Cypress.sinon.match({ type: 'loadSync', chunkName: 'letters-A' }))
+      .should('be.calledWith', Cypress.sinon.match({ type: 'loadSync', chunkName: 'letters-F' }));
+  })
+  it('passes with ssrfalse', () => {
+    cy.visit('/?entrypoint=ssrfalse', {
+    })
+    cy.window().then((win) => {
+      cy.spy(win, 'loadableEvents').as('spyloadableEvents')
+    })
+  
+
+    cy.get('@spyloadableEvents')
+    .should('be.calledWith', Cypress.sinon.match({ type: 'startAsyncLoad', chunkName: 'letters-E-param' }))
+      .should('be.calledWith', Cypress.sinon.match({ type: 'successAsyncLoad', chunkName: 'letters-E-param' }))
   })
 })
