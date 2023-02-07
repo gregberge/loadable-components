6d9bdece84df1e9c09a785a5df9e9870578c30f9
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2023-02-07 23:53:16 +0100

    Use helper in cypress tests

--- examples/server-side-rendering/cypress/tests/spec.cy.js
+++ examples/server-side-rendering/cypress/tests/spec.cy.js
@@ -1,37 +1,37 @@
-describe('template spec', () => {
-  it('passes with simple', () => {
-    cy.visit('/?entrypoint=simple', {
-    })
-    cy.window().then((win) => {
-      cy.spy(win, 'loadableEvents').as('spyloadableEvents')
-    })
-  
 
-    cy.get('@spyloadableEvents')
-      .should('be.calledWith', Cypress.sinon.match({ type: 'loadSync', chunkName: 'letters-A' }));
+const spy = () => {
+  cy.window().then((win) => {
+    cy.spy(win, 'loadableEvents').as('spyloadableEvents')
+  })
+}
+
+const shouldObserve = (event) => {
+  cy.get('@spyloadableEvents')
+    .should('be.calledWith', Cypress.sinon.match(event))
+}
+
+describe('test entrypoints', () => {
+  it('passes with simple', () => {
+    cy.visit('/?entrypoint=simple', {})
+
+    spy()
+
+    shouldObserve({ type: 'loadSync', chunkName: 'letters-A' })
   })
   it('passes with props', () => {
-    cy.visit('/?entrypoint=props', {
-    })
-    cy.window().then((win) => {
-      cy.spy(win, 'loadableEvents').as('spyloadableEvents')
-    })
-  
+    cy.visit('/?entrypoint=props', {})
 
-    cy.get('@spyloadableEvents')
-      .should('be.calledWith', Cypress.sinon.match({ type: 'loadSync', chunkName: 'letters-A' }))
-      .should('be.calledWith', Cypress.sinon.match({ type: 'loadSync', chunkName: 'letters-F' }));
+    spy()
+
+    shouldObserve({ type: 'loadSync', chunkName: 'letters-A' })
+    shouldObserve({ type: 'loadSync', chunkName: 'letters-F' })
   })
   it('passes with ssrfalse', () => {
-    cy.visit('/?entrypoint=ssrfalse', {
-    })
-    cy.window().then((win) => {
-      cy.spy(win, 'loadableEvents').as('spyloadableEvents')
-    })
-  
+    cy.visit('/?entrypoint=ssrfalse', {})
 
-    cy.get('@spyloadableEvents')
-    .should('be.calledWith', Cypress.sinon.match({ type: 'startAsyncLoad', chunkName: 'letters-E-param' }))
-      .should('be.calledWith', Cypress.sinon.match({ type: 'successAsyncLoad', chunkName: 'letters-E-param' }))
+    spy()
+  
+    shouldObserve({ type: 'startAsyncLoad', chunkName: 'letters-E-param' })
+    shouldObserve({ type: 'successAsyncLoad', chunkName: 'letters-E-param' })
   })
 })
