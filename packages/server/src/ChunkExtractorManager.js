b598f085b2506d4e57092a4627b81aa086f255f9
Valentin Semirulnik <v7rulnik@gmail.com>
2024-04-08 18:22:06 +0400

    Add extensions to all imports

--- packages/server/src/ChunkExtractorManager.js
+++ packages/server/src/ChunkExtractorManager.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Context } from './sharedInternals'
+import { Context } from './sharedInternals.js'
 
 const ChunkExtractorManager = ({ extractor, children }) => (
   <Context.Provider value={extractor}>{children}</Context.Provider>
