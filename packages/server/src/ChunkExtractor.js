gregberge/loadable-components

commit 114cea2b61ef644d3b794a9b5f48df69f0f5400b

fix: correct esm configuration for loadable/server, fixes: #999 (#1004)
* fix: correct esm configuration for loadable/server, fixes: #999

* chore: put common-js override in

* update snapshots

packages/server/src/ChunkExtractor.js
@@ -1,13 +1,13 @@
 /* eslint-disable react/no-danger */
 import path from 'path'
 import fs from 'fs'
-import uniq from 'lodash/uniq.js'
-import uniqBy from 'lodash/uniqBy.js'
-import flatMap from 'lodash/flatMap.js'
+import uniq from 'lodash/uniq'
+import uniqBy from 'lodash/uniqBy'
+import flatMap from 'lodash/flatMap'
 import React from 'react'
-import { invariant, getRequiredChunkKey } from './sharedInternals.js'
-import ChunkExtractorManager from './ChunkExtractorManager.js'
-import { smartRequire, joinURLPath, readJsonFileSync } from './util.js'
+import { invariant, getRequiredChunkKey } from './sharedInternals'
+import ChunkExtractorManager from './ChunkExtractorManager'
+import { smartRequire, joinURLPath, readJsonFileSync } from './util'
 
 const EXTENSION_SCRIPT_TYPES = {
   '.js': 'script',

packages/server/src/ChunkExtractorManager.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Context } from './sharedInternals.js'
+import { Context } from './sharedInternals'
 
 const ChunkExtractorManager = ({ extractor, children }) => (
   <Context.Provider value={extractor}>{children}</Context.Provider>

packages/server/src/index.js
@@ -1,2 +1,2 @@
-export { default as ChunkExtractorManager } from './ChunkExtractorManager.js'
-export { default as ChunkExtractor } from './ChunkExtractor.js'
+export { default as ChunkExtractorManager } from './ChunkExtractorManager'
+export { default as ChunkExtractor } from './ChunkExtractor'

packages/server/src/ChunkExtractor.test.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import 'regenerator-runtime/runtime.js'
+import 'regenerator-runtime/runtime'
 import path from 'path'
 import stats from '../__fixtures__/stats.json'
-import ChunkExtractor from './ChunkExtractor.js'
+import ChunkExtractor from './ChunkExtractor'
 
 const targetPath = path.resolve(
   __dirname,

packages/server/src/util.test.js
@@ -1,4 +1,4 @@
-import { joinURLPath } from './util.js'
+import { joinURLPath } from './util'
 
 describe('util', () => {
   describe('#joinURLPath', () => {

packages/server/rollup.config.js
@@ -1,0 +1,48 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import nodeResolve from 'rollup-plugin-node-resolve'
+import babel from 'rollup-plugin-babel'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+import pkg from './package.json'
+
+const input = 'src/index.js'
+const name = 'loadable'
+const globals = {
+  react: 'React',
+  'hoist-non-react-statics': 'hoistNonReactStatics',
+}
+
+const external = id => !id.startsWith('.') && !id.startsWith('/')
+
+const getBabelOptions = ({ useESModules }) => ({
+  exclude: '**/node_modules/**',
+  runtimeHelpers: true,
+  presets: [
+    ['@babel/preset-env', { loose: true }],
+    ['@babel/preset-react', { useBuiltIns: true }],
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    'babel-plugin-annotate-pure-calls',
+    ['@babel/plugin-transform-runtime', { useESModules }],
+  ],
+})
+
+export default [
+  // cjs
+  {
+    input,
+    output: { file: pkg.main, format: 'cjs', exports: 'named' },
+    external,
+    plugins: [babel(getBabelOptions({ useESModules: false })), sizeSnapshot()],
+  },
+  // esm
+  {
+    input,
+    output: { file: pkg.module, format: 'esm' },
+    external,
+    plugins: [babel(getBabelOptions({ useESModules: true })), sizeSnapshot()],
+  },
+]

commit 19241b9f49c19262ecb5d007d855036ccbabb37d

Bring back extensions in server package
Brings back extensions removed in 114cea2b61ef644d3b794a9b5f48df69f0f5400b, which caused issue #1005.

packages/server/src/ChunkExtractor.js
@@ -1,13 +1,13 @@
 /* eslint-disable react/no-danger */
 import path from 'path'
 import fs from 'fs'
-import uniq from 'lodash/uniq'
-import uniqBy from 'lodash/uniqBy'
-import flatMap from 'lodash/flatMap'
+import uniq from 'lodash/uniq.js'
+import uniqBy from 'lodash/uniqBy.js'
+import flatMap from 'lodash/flatMap.js'
 import React from 'react'
-import { invariant, getRequiredChunkKey } from './sharedInternals'
-import ChunkExtractorManager from './ChunkExtractorManager'
-import { smartRequire, joinURLPath, readJsonFileSync } from './util'
+import { invariant, getRequiredChunkKey } from './sharedInternals.js'
+import ChunkExtractorManager from './ChunkExtractorManager.js'
+import { smartRequire, joinURLPath, readJsonFileSync } from './util.js'
 
 const EXTENSION_SCRIPT_TYPES = {
   '.js': 'script',

packages/server/src/ChunkExtractorManager.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Context } from './sharedInternals'
+import { Context } from './sharedInternals.js'
 
 const ChunkExtractorManager = ({ extractor, children }) => (
   <Context.Provider value={extractor}>{children}</Context.Provider>

packages/server/src/index.js
@@ -1,2 +1,2 @@
-export { default as ChunkExtractorManager } from './ChunkExtractorManager'
-export { default as ChunkExtractor } from './ChunkExtractor'
+export { default as ChunkExtractorManager } from './ChunkExtractorManager.js'
+export { default as ChunkExtractor } from './ChunkExtractor.js'

packages/server/src/ChunkExtractor.test.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import 'regenerator-runtime/runtime'
+import 'regenerator-runtime/runtime.js'
 import path from 'path'
 import stats from '../__fixtures__/stats.json'
-import ChunkExtractor from './ChunkExtractor'
+import ChunkExtractor from './ChunkExtractor.js'
 
 const targetPath = path.resolve(
   __dirname,
@@ -683,7 +683,7 @@ describe('ChunkExtrator', () => {
       expect(extractor.inputFileSystem.readFile).toHaveBeenCalledTimes(2)
       expect(data).toMatchInlineSnapshot(`
         "foo
-        
+
         foo
         "
       `)

packages/server/src/util.test.js
@@ -1,4 +1,4 @@
-import { joinURLPath } from './util'
+import { joinURLPath } from './util.js'
 
 describe('util', () => {
   describe('#joinURLPath', () => {